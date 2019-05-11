27c4b8c00f37f78afc99295f2cff19ea2c31717e
Brian Rizqi <30972350+brianrizqi@users.noreply.github.com>
2019-05-11 12:21:07 +0700

    bug fixed

--- app/components/main.js
+++ app/components/main.js
@@ -53,6 +53,11 @@ export default class Main extends React.Component {
             this.setState({noteText: ''});
         }
     }
+
+    deleteNote(key) {
+        this.state.noteArray.splice(key, 1);
+        this.setState({noteArray: this.state.noteArray});
+    }
 }
 
 const styles = StyleSheet.create({
